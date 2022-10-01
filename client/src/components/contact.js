8462b01f4ee6c3b56cf2ec661bf868b966a4e5b4
curlerhaus <101598928+curlerhaus@users.noreply.github.com>
2022-10-01 16:47:36 -0500

    updated contact form

--- client/src/components/contact.js
+++ client/src/components/contact.js
@@ -1,6 +1,7 @@
 import Form from "react-bootstrap/Form";
 import TopNavBar from "./topnavbar";
 import "./contact.css";
+import Button from "react-bootstrap/Button";
 
 export default function Contact() {
   return (
@@ -10,12 +11,15 @@ export default function Contact() {
       <Form>
         <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
           <Form.Label>Email address</Form.Label>
-          <Form.Control type="email" placeholder="name@example.com" />
+          <Form.Control required type="email" placeholder="name@example.com" />
         </Form.Group>
         <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
           <Form.Label>Message</Form.Label>
           <Form.Control as="textarea" rows={3} />
         </Form.Group>
+        <Button variant="primary" type="submit">
+          Submit
+        </Button>
       </Form>
     </>
   );
