7b8f4a1523f6ccfb727760955044ce8e733d82b6
curlerhaus <101598928+curlerhaus@users.noreply.github.com>
2022-10-05 16:42:29 -0500

    update nav bar

--- client/src/components/topnavbar.js
+++ client/src/components/topnavbar.js
@@ -1,36 +1,46 @@
-import Container from 'react-bootstrap/Container';
-import Navbar from 'react-bootstrap/Navbar';
-import Form from 'react-bootstrap/Form';
-import Button from 'react-bootstrap/Button';
+import Container from "react-bootstrap/Container";
+import Navbar from "react-bootstrap/Navbar";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+import "./topnavbar.css";
 
 import { useNavigate } from "react-router-dom";
-
+import Nav from "react-bootstrap/Nav";
 
 function Topnavbar() {
+  const navigate = useNavigate();
 
-    const navigate = useNavigate();
+  return (
+    <div>
+      <Navbar bg="dark" variant="dark">
+        <Container>
+          <a href="/home" className="logo">
+            <img src="./logo/DTN_logo.png" alt="logo" />
+          </a>
+          <Nav className="me-auto">
+            <Nav.Link href="/contact">Contact</Nav.Link>
+            <Nav.Link href="/about">About Us</Nav.Link>
+          </Nav>
+          <Form className="d-flex">
+            <Form.Control
+              type="search"
+              placeholder="Search"
+              className="me-6"
+              aria-label="Search"
+            />
+            <Button variant="secondary">Search</Button>
+          </Form>
+        </Container>
 
-
-    return (
-        <div>
-            <Navbar bg="dark" variant="dark">
-                <Container>
-                    <Navbar.Brand href="#home">DTNS News Blog</Navbar.Brand>
-                    <Form.Control
-                        type="search"
-                        placeholder="Search"
-                        className="me-2"
-                        aria-label="Search"
-                    />
-                    <Button variant="secondary">Search</Button>
-                </Container>
-
-                <Button variant="secondary" onClick={() => navigate('/Login')}>Login</Button>
-                <Button variant="secondary" onClick={() => navigate('/signup')}>SignUp</Button>
-
-            </Navbar>
-        </div>
-    )
+        <Button variant="secondary" onClick={() => navigate("/Login")}>
+          Login
+        </Button>
+        <Button variant="secondary" onClick={() => navigate("/signup")}>
+          SignUp
+        </Button>
+      </Navbar>
+    </div>
+  );
 }
 
-export default Topnavbar
+export default Topnavbar;
